6043d1a493b0ddf0baa5c9d7e3f533ae90bda4a2
Anton Junaidi <madara_clan@live.com>
2013-02-22 23:29:28 +0700

    Fix Default Param

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -9,8 +9,19 @@ class Home extends MY_Controller {
 			array(
 				'css/front.css'
 			), "", "", "Home"
+		$this->_default_param(
+			"", "", "", "Home"
 		);
 		$this->load->view('t/home/index_view', $this->data);
+	}
+
+	function _default_param($css = array(), $js = array(), $meta = array(), $title = ""){
+		$default_css = array(
+			'css/front.css'
+		);
+		if (!empty($css)) $css = array_merge($default_css, $css);
+		//if (!empty($js)) $js = array_merge($default_js, $js);
+		$this->default_param($css, $js, $meta, $title);
 	}
 
 }
