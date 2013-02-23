85bca3fea8f2e01c6fb1f74a09c59db603ac1829
Anton Junaidi <madara_clan@live.com>
2013-02-23 15:35:32 +0700

    profile page

--- application/views/t/profile/user_profile_view.php
+++ application/views/t/profile/user_profile_view.php
@@ -1,9 +1,85 @@
 <?php $this->load->view('t/general/header_view', $this->data); ?>
 
-<div class="container">
+<div class="container container_content" style="margin-top:80px;">
 
-	
-	
+	<div class="gallery_profile">
+
+		<div class="fix_profile_block">
+			<div class="profile_block">
+				<div class="profile_block_wrapper">
+					<div class="profile_title">Anton's Profile</div>
+				</div>
+			</div>
+		</div>
+			
+		<div class="gallery">
+			<div class="ga_image">
+				<a href="#">
+					<img src="<?php echo cdn_url(); ?>images/photogallery.jpg" alt="photo gallery" />
+				</a>
+			</div>
+			<div class="ga_detail">
+				<div class="album_link"><a href="#">Album ke Bali</a></div>
+			</div>
+		</div>
+
+		<div class="gallery">
+			<div class="ga_image">
+				<a href="#">
+					<img src="<?php echo cdn_url(); ?>images/photogallery.jpg" alt="photo gallery" />
+				</a>
+			</div>
+			<div class="ga_detail">
+				<div class="album_link"><a href="#">Album ke Bali</a></div>
+			</div>
+		</div>
+
+		<div class="gallery">
+			<div class="ga_image">
+				<a href="#">
+					<img src="<?php echo cdn_url(); ?>images/photogallery.jpg" alt="photo gallery" />
+				</a>
+			</div>
+			<div class="ga_detail">
+				<div class="album_link"><a href="#">Album ke Bali</a></div>
+			</div>
+		</div>
+
+		<div class="gallery">
+			<div class="ga_image">
+				<a href="#">
+					<img src="<?php echo cdn_url(); ?>images/photogallery.jpg" alt="photo gallery" />
+				</a>
+			</div>
+			<div class="ga_detail">
+				<div class="album_link"><a href="#">Album ke Bali</a></div>
+			</div>
+		</div>
+
+		<div class="gallery">
+			<div class="ga_image">
+				<a href="#">
+					<img src="<?php echo cdn_url(); ?>images/photogallery.jpg" alt="photo gallery" />
+				</a>
+			</div>
+			<div class="ga_detail">
+				<div class="album_link"><a href="#">Album ke Bali</a></div>
+			</div>
+		</div>
+
+		<div class="gallery">
+			<div class="ga_image">
+				<a href="#">
+					<img src="<?php echo cdn_url(); ?>images/photogallery.jpg" alt="photo gallery" />
+				</a>
+			</div>
+			<div class="ga_detail">
+				<div class="album_link"><a href="#">Album ke Bali</a></div>
+			</div>
+		</div>
+
+	</div>
+
 </div>
 
 <?php $this->load->view('t/general/footer_view', $this->data); ?>
